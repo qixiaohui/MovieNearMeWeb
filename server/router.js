b4d46e1dae86219f60a3eb4274e1a9f3e50feade
qixiaohui <qixiaohuihaha@gmail.com>
2016-09-27 11:51:37 -0700

    fix path naming issue

--- server/router.js
+++ server/router.js
@@ -6,7 +6,7 @@ const router = express.Router();
 const moviesGateway = require('./rest/MoviesGateway/movies_gateway');
 const movieInfoGateway = require('./rest/MovieInfoGateway/movie_info_gateway');
 const movieScheduleGateway = require('./rest/MovieScheduleGateway/movie_schedule_gateway');
-const newsGateway = require('./rest/newsGateway/news_gateway');
+const newsGateway = require('./rest/NewsGateway/news_gateway');
 
 router.use('/movies', moviesGateway);
 router.use('/movie', movieInfoGateway);
